01035cd5692643a8b2b579066f9d7c803f82bf43
Daniel Ballesteros <danielballesteros4@gmail.com>
2025-10-02 02:22:09 +0200

    - Add AsyncUploadServiceImpl

---
+++ src/main/java/org/dballesteros/filemanager/application/service/impl/AsyncUploadServiceImpl.java
@@ -0,0 +1,27 @@
+package org.dballesteros.filemanager.application.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.dballesteros.filemanager.application.service.AsyncUploadService;
+import org.dballesteros.filemanager.domain.model.AssetDto;
+import org.dballesteros.filemanager.domain.port.client.UploadClientPort;
+import org.dballesteros.filemanager.domain.port.repository.AssetRepositoryPort;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class AsyncUploadServiceImpl implements AsyncUploadService {
+    private final AssetRepositoryPort assetRepository;
+    private final UploadClientPort uploadClientPort;
+
+
+    @Async("uploadExecutor")
+    public void uploadFileAsync(final AssetDto assetDto) {
+        this.uploadClientPort.uploadFile(assetDto)
+                .map(this.assetRepository::update)
+                .then()
+                .toFuture();
+    }
+}
